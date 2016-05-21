bc1f9d48a326ac7df04745258deb17b5f7dfded9
lbm <am003507@gmail.com>
2016-05-21 22:35:39 +0900

    sdafasdfd

--- ddokdy/src/main/java/com/study/ddokdy/MainActivity.java
+++ ddokdy/src/main/java/com/study/ddokdy/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends android.support.v4.app.FragmentActivity {
 
 	private void init() {
 		ZMethod.setStatusColor(this, Color.parseColor(Config.Color_orange));
-		Toast.makeText(getApplicationContext(),"asdfasdf",Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(),"adsfasdfasdf",Toast.LENGTH_SHORT).show();
 	}
 	private void holdViews() {
 		
