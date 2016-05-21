75f4c3c9b568e45def86a150eae950626058348d
lbm <am003507@gmail.com>
2016-05-21 23:24:14 +0900

    fixing .gitignore

--- ddokdy/src/main/java/com/study/ddokdy/MainActivity.java
+++ ddokdy/src/main/java/com/study/ddokdy/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends android.support.v4.app.FragmentActivity {
 
 	private void init() {
 		ZMethod.setStatusColor(this, Color.parseColor(Config.Color_orange));
-		ZMethod.toast(this, "어휴mkm");
+		ZMethod.toast(this, "dㅁㄴasdfasdfasdfㅇ");
 	}
 	private void holdViews() {
 		
